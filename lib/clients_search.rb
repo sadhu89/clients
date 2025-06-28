44f2e9d0a3173ff37cedcf32e0866bcd6f490d75
Carlos Rojas <carlosrojas@Remote-Carlos-Rojas.local>
2025-06-29 09:39:24 +1000

    Add new command for search

--- lib/clients_search.rb
+++ lib/clients_search.rb
@@ -1,8 +1,38 @@
 require 'thor'
+require 'json'
 
 class ClientsSearch < Thor
-  desc "greet", "Print hello world message"
-  def greet
-    puts "Hello World!"
+  desc "search QUERY", "Search through all clients in clients.json and return those with names partially matching a given search query"
+  def search(query)
+    clients_file = File.join(File.dirname(__FILE__), '..', 'data', 'clients.json')
+    
+    unless File.exist?(clients_file)
+      puts "Error: clients.json file not found at #{clients_file}"
+      exit 1
+    end
+
+    begin
+      clients_data = JSON.parse(File.read(clients_file))
+    rescue JSON::ParserError => e
+      puts "Error: Invalid JSON in clients.json file: #{e.message}"
+      exit 1
+    end
+
+    matching_clients = clients_data.select do |client|
+      client['full_name']&.downcase&.include?(query.downcase)
+    end
+
+    if matching_clients.empty?
+      puts "No clients found matching '#{query}'"
+    else
+      puts "Found #{matching_clients.length} client(s) matching '#{query}':"
+      puts
+      matching_clients.each do |client|
+        puts "ID: #{client['id']}"
+        puts "Name: #{client['full_name']}"
+        puts "Email: #{client['email']}"
+        puts "-" * 40
+      end
+    end
   end
 end
--- spec/clients_search_spec.rb
+++ spec/clients_search_spec.rb
@@ -2,24 +2,4 @@ require 'spec_helper'
 require_relative '../lib/clients_search'
 
 RSpec.describe ClientsSearch do
-  describe '#greet' do
-    it 'prints hello world message' do
-      expect { subject.greet }.to output("Hello World!\n").to_stdout
-    end
-
-    it 'returns nil' do
-      expect(subject.greet).to be_nil
-    end
-  end
-
-  describe 'command line interface' do
-    it 'responds to greet command' do
-      expect(ClientsSearch.new).to respond_to(:greet)
-    end
-
-    it 'has greet as a documented command' do
-      commands = ClientsSearch.tasks.keys
-      expect(commands).to include('greet')
-    end
-  end
 end 
